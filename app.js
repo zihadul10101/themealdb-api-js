bb5bb176a21843a56787b262ac50b0776b2466c3
unknown <you@example.com>
2021-02-08 21:49:06 +0600

    meal complit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const searchText = () => {
     const foodText = document.getElementById('inputValue').value;
    // console.log(foodText);
+
        const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodText}` 
        fetch(url)
         .then(res => res.json())
@@ -13,29 +14,45 @@ const searchText = () => {
 const displayFoods = meal => {
  // console.log(meal)
    const foodContainer=document.getElementById('food-container');
-
+if(meal==null){
+  alert('error')
+}
+else{
   meal.forEach(meal=>{
-     const foodDiv=document.createElement('div');
-     foodDiv.className='foodName';
-     foodDiv.innerHTML=`
+    const foodDiv=document.createElement('div');
+    foodDiv.className='foodName';
+    foodDiv.innerHTML=`
 
-     <div class="meal">
-     <img onClick="foodDetails('${meal.strMeal}')"class="img" src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
-     <div class="meal-info" data-mealID="${meal.idMeal}">
-     <h3>${meal.strMeal}</h3></div>
+    <div class="meal">
+    <img onClick="foodDetails('${meal.idMeal}')"class="img" src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+    <div class="meal-info" data-mealID="${meal.idMeal}">
+    <h3>${meal.strMeal}</h3></div>
 </div>
-     `;
- foodContainer.appendChild(foodDiv);
+    `;
+foodContainer.appendChild(foodDiv);
 
-  })
+ })
+}
+  
 //   
 }
-
+//const url=`https://www.themealdb.com/api/json/v1/1/list.php?i=${strMeal}`
 const foodDetails=strMeal=>{
-   const url=`https://www.themealdb.com/api/json/v1/1/list.php?c=${strMeal}`
-  // console.log(url);
+   const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${strMeal}`
+  console.log(url);
   fetch(url)
    .then(res=>res.json())
-    .then(data=>console.log(data[0].meals))
+    .then(data=>foodDetailsInfo(data.meals[0]))
    
+}
+const foodDetailsInfo=foodName=>{
+  console.log(foodName);
+  const foodDiv=document.getElementById('foodDetils');
+  foodDiv.innerHTML=`
+  <div class="details">
+  <h1>${foodName.strMeal}</h1>
+     <li>${foodName.strInstructions}
+     </li>
+     </div>
+  `
 }
