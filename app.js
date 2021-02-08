757e656dbc2d0177e1448859d16764817d55ebd1
unknown <you@example.com>
2021-02-08 22:46:44 +0600

    meals done

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const displayFoods = meal => {
       foodDiv.innerHTML = `
 
     <div class="meal">
-    <img onClick="foodDetails('${meal.idMeal}')"class="img" src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+    <img onClick="mealDetails('${meal.idMeal}')"class="img" src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
     <div class="meal-info" data-mealID="${meal.idMeal}">
     <h3>${meal.strMeal}</h3></div>
 </div>
@@ -38,22 +38,24 @@ const displayFoods = meal => {
 }
 
 // load meals details
-const foodDetails = strMeal => {
+const mealDetails = strMeal => {
   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${strMeal}`
   console.log(url);
   fetch(url)
     .then(res => res.json())
-    .then(data => foodDetailsInfo(data.meals[0]))
+    .then(data => mealDetailsInfo(data.meals[0]))
 
 }
-const foodDetailsInfo = foodName => {
+const mealDetailsInfo = foodName => {
   console.log(foodName);
   const foodDiv = document.getElementById('foodDetils');
   foodDiv.innerHTML = `
   <div class="details">
   <h1>${foodName.strMeal}</h1>
-     <li>${foodName.strInstructions}
-     </li>
+     <p>
+     <li>${foodName.strInstructions}</li>
+     
+     </p>
      </div>
   `
 }
