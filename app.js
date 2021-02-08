aa3eb915ee81f51997f155a8f76859166e884a01
unknown <you@example.com>
2021-02-08 20:06:23 +0600

    meal finder

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const search = document.getElementById("suarch");
-const submit = document.getElementById("submit");
-const mealsEl = document.getElementById("meals");
-const resultHeading = document.getElementById("result-heading");
-const singleMeal = document.getElementById("single-meal");
 
 
+const searchText = () => {
+    const foodText = document.getElementById('inputValue').value;
+   // console.log(foodText);
+       const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodText}` 
+       fetch(url)
+        .then(res => res.json())
+        .then(data => displayFoods(data.meals))
 
-submit.addEventListener("click", function () {
-    resultHeading.innerHTML = "";
 
-    const term = document.getElementById("Search").value;
-    console.log(term);
-    if (term.trim()) {
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                resultHeading.innerHTML = `<h2>Search results for '${term}': <h2>`
-                if (data === null) {
-                    resultHeading.innerHTML = `
-<p>There are no search results.Try again!</p>
-`   } else {
-                    mealsEl.innerHTML = data.meals
-                        .map(
-                            (meal) => `
-<div class="meal">
-<img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
-<div class="meal-info" data-mealID="${meal.idMeal}">
-<h3>${meal.strMeal}</h3></div>
-`)
-                        .json("");
-                }
-            });
-        const valuen = document.getElementById("Search").value = "";
-        console.log(valuen);
-    }
-    else {
-        alert("Please enter a search term");
-    }
-});
+}
+const displayFoods = meal => {
+ // console.log(meal)
+   const foodContainer=document.getElementById('food-container');
+
+  meal.forEach(meal=>{
+     const foodDiv=document.createElement('div');
+     foodDiv.className='foodName';
+     foodDiv.innerHTML=`
+
+     <div class="meal">
+     <img onClick="foodDetails('${meal.strMeal}')"class="img" src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+     <div class="meal-info" data-mealID="${meal.idMeal}">
+     <h3>${meal.strMeal}</h3></div>
+</div>
+     `;
+ foodContainer.appendChild(foodDiv);
+
+  })
+//   
+}
+
+const foodDetails=strMeal=>{
+   const url=`https://www.themealdb.com/api/json/v1/1/list.php?c=${strMeal}`
+  // console.log(url);
+  fetch(url)
+   .then(res=>res.json())
+    .then(data=>console.log(data[0].meals))
+   
+}
