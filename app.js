94ab04ca90c58c1a4625ce4532ec606314d031af
unknown <you@example.com>
2021-02-08 22:18:45 +0600

    done

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 
 
 const searchText = () => {
-    const foodText = document.getElementById('inputValue').value;
-   // console.log(foodText);
-
-       const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodText}` 
-       fetch(url)
-        .then(res => res.json())
-        .then(data => displayFoods(data.meals))
+  const foodText = document.getElementById('inputValue').value;
+  // console.log(foodText);
+  //load data
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodText}`
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayFoods(data.meals))
 
 
 }
 const displayFoods = meal => {
- // console.log(meal)
-   const foodContainer=document.getElementById('food-container');
-if(meal==null){
-  alert('error')
-}
-else{
-  meal.forEach(meal=>{
-    const foodDiv=document.createElement('div');
-    foodDiv.className='foodName';
-    foodDiv.innerHTML=`
+  // console.log(meal)
+  const foodContainer = document.getElementById('food-container');
+  if (meal == null) {
+    alert('Meal name wrong!')
+  }
+  else {
+    meal.forEach(meal => {
+      const foodDiv = document.createElement('div');
+      foodDiv.className = 'foodName';
+      foodDiv.innerHTML = `
 
     <div class="meal">
     <img onClick="foodDetails('${meal.idMeal}')"class="img" src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
@@ -29,26 +29,27 @@ else{
     <h3>${meal.strMeal}</h3></div>
 </div>
     `;
-foodContainer.appendChild(foodDiv);
+      foodContainer.appendChild(foodDiv);
 
- })
+    })
+  }
+
+  //   
 }
-  
-//   
-}
-//const url=`https://www.themealdb.com/api/json/v1/1/list.php?i=${strMeal}`
-const foodDetails=strMeal=>{
-   const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${strMeal}`
+
+// load meals details
+const foodDetails = strMeal => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${strMeal}`
   console.log(url);
   fetch(url)
-   .then(res=>res.json())
-    .then(data=>foodDetailsInfo(data.meals[0]))
-   
+    .then(res => res.json())
+    .then(data => foodDetailsInfo(data.meals[0]))
+
 }
-const foodDetailsInfo=foodName=>{
+const foodDetailsInfo = foodName => {
   console.log(foodName);
-  const foodDiv=document.getElementById('foodDetils');
-  foodDiv.innerHTML=`
+  const foodDiv = document.getElementById('foodDetils');
+  foodDiv.innerHTML = `
   <div class="details">
   <h1>${foodName.strMeal}</h1>
      <li>${foodName.strInstructions}
